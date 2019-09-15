2b0d91a7f33ac8e2480ed601d63d31c4b7b405e8
Elliot <hylee101001@gmail.com>
2019-09-14 19:41:38 -0700

    asdfasdf

---
+++ main.py
@@ -0,0 +1,13 @@
+from pycocotools.coco import COCO
+import numpy as np
+import skimage.io as io
+import matplotlib.pyplot as plt
+import pylab
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
