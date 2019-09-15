6ac3fd54c2741071b7a5722cff8f5bf3805046e1
el <hylee101001@gmail.com>
2019-09-14 21:31:58 -0700

    aaa

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ def main():
     imgIds = t.coco.getImgIds()
 
     N = len(imgIds)
-    for i in range(0, 10):
+    for i in range(0, N):
         I = t.drawBBox(i)
         cv2.imshow("asdf", I)
-        cv2.waitKey(1000)
+        cv2.waitKey(3000)
 
 
 
--- util.py
+++ util.py
@@ -23,19 +23,31 @@ class Tools:
 
     def getBBox(self, imgId):
         anns = self.getAnns(imgId)
+        objNames = self.getCatNames(anns)
         N = len(anns)
         bboxes = np.zeros((N, 4), dtype=int)
         for i in range(0, N):
             bboxes[i,:] = np.array(anns[i]['bbox'], dtype = int)
-        return bboxes
+        return bboxes, objNames
 
-    def drawBBox(self, index):
+    def getCatNames(self, anns):
+        class_id = []
+        for i in range(0, len(anns)):
+            class_id.append([x['name'] for x in self.cats if x['id'] == anns[i]['category_id']][0])
+        return class_id
+
+    def getImgAnns(self, index):
         I = self.getImgUrl(index)
         imgInfo = self.coco.loadImgs(self.imgIds)[index]
-        bboxes = self.getBBox(imgInfo['id'])
+        bboxes, objNames = self.getBBox(imgInfo['id'])
+        return I, bboxes, objNames
+
+    def drawBBox(self, index):
+        I, bboxes, objNames = self.getImgAnns(index)
         for i in range(0, bboxes.shape[0]):
             x, y = bboxes[i, 0], bboxes[i, 1]
             x2, y2 = x+bboxes[i, 2], y+bboxes[i, 3]
-            I = cv2.rectangle(I, (x, y), (x2, y2), (255, 0, 0), 2)
+            I = cv2.rectangle(I, (x, y), (x2, y2), (0, 0, 255), 2)
+            I = cv2.putText(I, objNames[i], (x, y-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
         return I
 
